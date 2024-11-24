a8a1e590aed6a8a20fc7d0d192f63b7a8d4a88dd
Kekgaugetswe <Kekgaugetswe@gmail.com>
2024-11-24 11:22:48 +0200

    Removed API directory from ui branch

--- API/JwtAuth.Domain/Models/DTO/CreateRoleRequestDto.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace JwtAuth.Domain.Models.DTO;
-
-public class CreateRoleRequestDto
-{
-    public string RoleName  {get; set;} = string.Empty;
-}
--- API/JwtAuth.Infrastructure/Repositories/ITokenRepository.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace JwtAuth.Infrastructure.Repositories;
-public interface ITokenRepository
-{
-    string CreateJwtToken(IdentityUser user, List<string> roles);
-}
